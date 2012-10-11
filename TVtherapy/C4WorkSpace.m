5417e9e1f057a344aefe0997ee0ec9d5ec88a830
dubpixel <i@dubpixel.tv>
2012-10-11 11:00:49 -0700

    Initial Commit

---
+++ TVtherapy/C4WorkSpace.m
@@ -0,0 +1,17 @@
+//
+//  C4WorkSpace.m
+//  TVtherapy
+//
+//  Created by YourMom on 10/11/12.
+//  Copyright (c) 2012 dubpixel inc. All rights reserved.
+//
+
+#import "C4WorkSpace.h"
+
+@implementation C4WorkSpace
+
+-(void)setup {
+	//work your magic here
+}
+					
+@end
---
+++ TVtherapy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TVtherapy
+//
+//  Created by YourMom on 10/11/12.
+//  Copyright (c) 2012 dubpixel inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "C4AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+    	return UIApplicationMain(argc, argv, nil, NSStringFromClass([C4AppDelegate class]));
+    }
+}
